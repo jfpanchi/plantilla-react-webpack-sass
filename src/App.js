d2d0e5747bbd1f2b5242194ae87660d08a55fbe6
jfpanchi2 <jfpanchi2@espe.edu.ec>
2023-01-28 16:17:34 -0500

    Configuracion Inicial React Webpack Sass

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="app">
+        <h1>React + Webpacl + SASS</h1>
+        <p>Proyecto</p>
+    </div>
+  )
+}
+
+export default App
